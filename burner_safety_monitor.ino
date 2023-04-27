fd0d7b67f15801d27c0ee34e82e1736a06ad2f38
Ethan Monka <90052898+ejmonka@users.noreply.github.com>
2023-04-26 20:07:54 -0500

    Update burner_safety_monitor.ino

--- burner_safety_monitor.ino
+++ burner_safety_monitor.ino
@@ -2,20 +2,28 @@
 #include <avr/wdt.h>
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
-#include <string>
 
 
-Adafruit_MLX90614 temp_sens = Adafruit_MLX90614();
+static const int CURRENT_SENS_PIN = A0;
+static const int BATTERY_PIN = A1;
+static const int SOLENOID_PIN = 8;
+static const int motion_sensor_in = 9;
+static const int motion_i = 0;
+static const float UNATTENDED_MAX_TIME = 90.0;
+static const float SHUTDOWN_MAX_TIME = 60.0;
+
+Adafruit_MLX90614 temp_sens = Adafruit_MLX90614(); // A4 == SCA, A5 == SCL
 SoftwareSerial BT(10,11);
-CURRENT_SENS_PIN = A0;
-BATTERY_PIN = A1;
-SOLENOID_PIN = D0;
 
-int motion_sensor_in = 9;
-int motion_i = 0;
+float unattended_timer = UNATTENDED_MAX_TIME;
+float shutdown_timer = SHUTDOWN_MAX_TIME;
 
 bool sign_in(String user_name, String password) {
   return false;
+}
+
+float getVoltage(int pin) {
+  return (float)analogRead(pin) * (5.0/1024);
 }
 
 bool send_response(){
@@ -53,22 +61,28 @@ bool isBatteryCharged() {
   return false;
 }
 
-void notifyUser(string msg){
+void notifyUser(String msg){
   BT.println(msg);
 }
 
-string getResponse(int timeout){
-  string response = "";
+String getResponse(){
+  String response = "";
   while(BT.available())
-    response += BT.read();
+    response += (char)BT.read();
   return response;
 }
 
 bool turnOffStove(){
-  return false;
+  digitalWrite(SOLENOID_PIN, HIGH); // Will change if normally closed or open
+  if(getVoltage(CURRENT_SENS_PIN) > 0)// Will this work? May have to change to interrupt/event
+    return false;
+  return true;
 }
 
 bool turnOnStove(){
+  digitalWrite(SOLENOID_PIN, HIGH); // Will change if normally closed or open
+  if(getVoltage(CURRENT_SENS_PIN) > 0)
+    return true;
   return false;
 }
 
@@ -77,46 +91,51 @@ bool restartStove(){
 }
 
 void motionISR(){
-  
+  unattended_timer = UNATTENDED_MAX_TIME;
+  shutdown_timer = SHUTDOWN_MAX_TIME;
+  Serial.println("Motion Detected");
 }
 
 void setup() {
   Serial.begin(9600);
-  wdt.disable();
+  wdt_disable();
   delay(2000);
-  wdt.enable(WDTO_2S);
+  wdt_enable(WDTO_2S);
   temp_sens.begin();
   BT.begin(9600);
-  pinMode(sensor_in, INPUT_PULLUP);
+  pinMode(motion_sensor_in, INPUT_PULLUP);// Motion Sensor
   pinMode(CURRENT_SENS_PIN, INPUT);// Current Sensor
   pinMode(BATTERY_PIN, INPUT);// Battery Voltage
-  pinMode(SOLENOID_PIN, OUTPUT;// Output to solenoid
+  pinMode(SOLENOID_PIN, OUTPUT);// Output to solenoid
+
+  attachInterrupt(digitalPinToInterrupt(motion_sensor_in), motionISR, RISING);
   
-  if(!checkTempSens())
+  //if(!checkTempSens())
     // handle temp sensor error
-  if(!checkMotionSens())
+  //if(!checkMotionSens())
     // handle motion sensor error
-  if(!checkCurrentSens())
+  //if(!checkCurrentSens())
     // handle current sensor error
+  Serial.println("System Reset");
 }
 
 void loop() {
   wdt_reset();
-  int j = digitalRead(sensor_in);
+  //int j = digitalRead(motion_sensor_in);
+  //Serial.println(j);
+  //delay(200);
+  
   if(BT.available()){
-    Serial.println(BT.read());
+    //Serial.println(BT.read());
+    Serial.println(getResponse());
     BT.println("Data received");
 
     
   }
   else{
-    BT.println("Hello from arduino");
+    BT.println(getTemp());
 
-    if(i != j){
-      Serial.println(j);
-    }
-    i = j; 
-    delay(500);
+    delay(400);
   }
-
+  
 }
