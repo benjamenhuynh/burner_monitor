dd9c372e88b2b43e47ec8d52d81c6bfaa8cb21c1
Ethan Monka <90052898+ejmonka@users.noreply.github.com>
2023-05-03 20:18:54 -0500

    Update burner_safety_monitor.ino

--- burner_safety_monitor.ino
+++ burner_safety_monitor.ino
@@ -2,26 +2,30 @@
 #include <avr/wdt.h>
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
-#include <string>
-#include <millisDelay.h>
 
 
-Adafruit_MLX90614 temp_sens = Adafruit_MLX90614();
+static const int CURRENT_SENS_PIN = A0;
+static const int BATTERY_PIN = A1;
+static const int SOLENOID_PIN = 8;
+static const int motion_sensor_in = 9;
+static const int motion_i = 0;
+static const unsigned long UNATTENDED_MAX_TIME = 5000;
+static const unsigned long SHUTDOWN_MAX_TIME = 5000;
+
+Adafruit_MLX90614 temp_sens = Adafruit_MLX90614(); // A4 == SCA, A5 == SCL
 SoftwareSerial BT(10,11);
-CURRENT_SENS_PIN = A0;
-BATTERY_PIN = A1;
-SOLENOID_PIN = D0;
 
-int motion_sensor_in = 9;
-int motion_i = 0;
-
+unsigned long unattended_timer = UNATTENDED_MAX_TIME;
+unsigned long shutdown_timer = SHUTDOWN_MAX_TIME;
+unsigned long prev_time = 0;
 unsigned long delayStart = 0;
-bool delayRunning = false;
-
-char * typeNamep[] = {"Object", "Ambient"};
 
 bool sign_in(String user_name, String password) {
   return false;
+}
+
+float getVoltage(int pin) {
+  return (float)analogRead(pin) * (5.0/1024);
 }
 
 bool send_response(){
@@ -53,6 +57,17 @@ bool checkMotionSens() {
   if(j > 0)
     return true; //if j returns a 1 that means we detect motion
   return false; //return false if nobody is there 
+  /*
+  unattended_timer = 0;
+  notifyUser("Please move in front of the sensor");
+  for(int i = 0; i < 10; i++) {
+    delay(400);
+    wdt_reset();
+    if(unattended_timer == UNATTENDED_MAX_TIME)
+      return true;
+  }
+  */
+  return false;
 }
 
 bool checkCurrentSens() {
@@ -60,86 +75,110 @@ bool checkCurrentSens() {
 }
 
 bool isBatteryCharged() {
-  return false;
+  if(getVoltage(BATTERY_PIN) < 2.59)
+      return false;
+  return true;
 }
 
-void notifyUser(string msg){
+void notifyUser(String msg){
   BT.println(msg);
 }
 
-string getResponse(int timeout){
-  string response = "";
+String getResponse(){
+  String response = "";
+  delay(20);
   while(BT.available())
     response += (char)BT.read();
   return response;
 }
 
 bool turnOffStove(){
-  
-  return false;
+  digitalWrite(SOLENOID_PIN, HIGH); // Will change if normally closed or open
+  //if(getVoltage(CURRENT_SENS_PIN) > 0)// Will this work? May have to change to interrupt/event
+  //  return false;
+  return true;
 }
 
 bool turnOnStove(){
-  return false;
+  digitalWrite(SOLENOID_PIN, LOW); // Will change if normally closed or open
+  //if(getVoltage(CURRENT_SENS_PIN) > 0)
+  //  return true;
+  return true;//false;
 }
 
 bool restartStove(){
-  //not sure what we can do here. 
-  return false;
+  notifyUser("The stove has been shut off. Please make sure the burners are turned off and select \"Ok\"");
+  String response = "";
+  while(response != "Ok\r\n")
+    response = getResponse();
+  return turnOnStove();
 }
 
+/*
 void motionISR(){
-  
+  unattended_timer = UNATTENDED_MAX_TIME;
+  shutdown_timer = SHUTDOWN_MAX_TIME;
+  Serial.println("Motion Detected");
 }
+*/
 
 void setup() {
   Serial.begin(9600);
-  wdt.disable();
+  wdt_disable();
   delay(2000);
-  wdt.enable(WDTO_2S);
+  wdt_enable(WDTO_2S);
   temp_sens.begin();
   BT.begin(9600);
-  pinMode(sensor_in, INPUT_PULLUP);
+  pinMode(motion_sensor_in, INPUT_PULLUP);// Motion Sensor
   pinMode(CURRENT_SENS_PIN, INPUT);// Current Sensor
   pinMode(BATTERY_PIN, INPUT);// Battery Voltage
-  pinMode(SOLENOID_PIN, OUTPUT;// Output to solenoid
+  pinMode(SOLENOID_PIN, OUTPUT);// Output to solenoid
 
-  delayStart =  millis();
-  delayRunning = true;
-
-  Serial.println("Robojax MLX90614 test");
-  if(!checkTempSens())
+  //attachInterrupt(digitalPinToInterrupt(motion_sensor_in), motionISR, RISING);
+  delayStart = millis();
+  bool delayRunning = false;
+  
+  //if(!checkTempSens())
     // handle temp sensor error
-  if(!checkMotionSens())
+  //if(!checkMotionSens())
     // handle motion sensor error
-  if(!checkCurrentSens())
+  //if(!checkCurrentSens())
     // handle current sensor error
+  Serial.println("System Reset");
 }
 
 void loop() {
   wdt_reset();
-  //int j = digitalRead(motion_sensor_in);
-  //check if temp is on
-
+  /*
+  while(!isStoveOn()) {
+    delay(400);
+    wdt_reset();
+  }
+  */
   if(isStoveOn())
   {
     if(!checkMotionSens()){
-        notifyUser("Stove is on, there is no motion detected. Should we turn the stove off? Reply with Y/N");
-        response = getResponse(2000);
-        while(response == NULL)
-        {
+        //notifyUser("Stove is on, there is no motion detected. Should we turn the stove off? Reply with Y/N");
+        //String response = getResponse();
+        String response = "";
+        do {
           notifyUser("Stove is on, there is no motion detected. Should we turn the stove off? Reply with Y/N");
-          response = getResponse(2000);
+          while(response != "Y\r\n" && response != "N\r\n"){
+            response = getResponse();
+            if(response != "")
+              Serial.println(response);
+            wdt_reset();
+          }
 
-          if(response == "Y")
+          if(response == "Y\r\n")
           {
             turnOffStove();
             notifyUser("Stove has been turned off, thanks!");
           }
-          elif(response == "N"){
+          else if(response == "N\r\n"){
              notifyUser("Stove will be kept on");
           }
-        }
+        } while(response == NULL);
     }
     else
     {
@@ -149,25 +188,26 @@ void loop() {
   else
   {
     notifyUser("Stove is not on");
+    while(!isStoveOn())
+      wdt_reset();
   }
-  //we need to check motion sensor
-  //then check if BT is available then send message
-  //if(BT.available()){
+  
+  /*
+  //int j = digitalRead(motion_sensor_in);
+  //Serial.println(j);
+  //delay(200);
+  
+  if(BT.available()){
     //Serial.println(BT.read());
-    //BT.println("Data received");
+    Serial.println(getResponse());
+    BT.println("Data received");
 
     
-  //}
-  //BT works, so does getTemp, motion sensor needs to interact with ISR. 
-  //Y/N feature for arduino to know if somebody is nearby.
-  /*else{
-    BT.println("Hello from arduino");
+  }
+  else{
+    BT.println(getTemp());
 
-    if(motion_i != j){
-      Serial.println(j);
-    }
-    motion_i = j; 
-    delay(500);
-  }*/
-
+    delay(400);
+  }
+  */
 }
